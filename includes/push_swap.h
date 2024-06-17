f193e9e11fcfae48e32df9344b2726e4ede39434
Joaquin Farnos Belles <jfarnos-@c1r8s4.42madrid.com>
2024-06-17 06:59:38 +0200

    push_swap commands

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: jfarnos- <jfarnos-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/25 20:50:40 by jfarnos-          #+#    #+#             */
-/*   Updated: 2024/06/13 21:45:48 by jfarnos-         ###   ########.fr       */
+/*   Updated: 2024/06/17 06:40:55 by jfarnos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,20 @@
 # include "./Libft/libft.h"
 
 int		check_nonvalid_char(char *argv);
-t_list *check_str_format(char *str);
 void	ft_error(char *string_error);
 void 	check_duplicated_int(t_list *stack);
 void    print_list(t_list *stack);
+void sa(t_list *stack_a);
+void sb(t_list *stack_b);
+void ss(t_list *stack_a, t_list *stack_b);
+void pa(t_list **stack_a, t_list **stack_b);
+void pb(t_list **stack_b, t_list **stack_a);
+void ra(t_list **stack_a);
+void rb(t_list **stack_b);
+void rr(t_list **stack_a, t_list **stack_b);
+void rra(t_list **stack_a);
+void rrb(t_list **stack_b);
+void rrr(t_list **stack_a, t_list **stack_b);
+t_list *check_str_format(char *str);
 
 #endif
